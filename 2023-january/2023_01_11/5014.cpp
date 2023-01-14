0e42f38ce401cf63d4910b3aa8755b5eac658fc0
suddiyo <suddii01@sju.ac.kr>
2023-01-14 23:41:54 +0900

    2023_01_11

--- 2023-january/2023_01_11/5014.cpp
+++ 2023-january/2023_01_11/5014.cpp
@@ -1,8 +1,38 @@
+// baekjoon 5014
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
+int F, S, G, U, D;
 
+int visited[1000001];
+
+void BFS() {
+    queue<int> Q;
+    Q.push(S);  // 강호가 있는 곳
+    visited[S] = 1;
+
+    while (!Q.empty()) {
+        int cur = Q.front();
+        Q.pop();
+
+        if (cur + U <= F && visited[cur + U] == 0) {
+            visited[cur + U] = visited[cur] + 1;
+            Q.push(cur + U);
+        }
+        if (cur - D > 0 && visited[cur - D] == 0) {
+            visited[cur - D] = visited[cur] + 1;
+            Q.push(cur - D);
+        }
+    }
+
+}
+
+int main() {
+    cin >> F >> S >> G >> U >> D;
+    BFS();
+    if (visited[G] == 0) cout << "use the stairs";
+    else cout << visited[G] - 1;
     return 0;
 }
