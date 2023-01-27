c5b38d1b213c58bf5cf8f43b9e128e64282cf475
suddiyo <suddii01@sju.ac.kr>
2023-01-27 19:22:05 +0900

    2023_01_23

--- 2023-january/2023_01_23/week2-1.cpp
+++ 2023-january/2023_01_23/week2-1.cpp
@@ -5,7 +5,16 @@
 
 using namespace std;
 
-int main() {
+bool solution(string s) {
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            st.push(s[i]);
+        } else if (s[i] == ')') {
+            if (st.empty()) return false;
+            else st.pop();
+        }
+    }
 
-    return 0;
+    return st.empty();
 }
