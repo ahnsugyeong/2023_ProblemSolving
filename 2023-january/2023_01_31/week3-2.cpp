45b5bc37ff7a54161af8be0672ef7a174ab24ba0
suddiyo <suddii01@sju.ac.kr>
2023-01-31 23:33:44 +0900

    2023_01_31

---
+++ 2023-january/2023_01_31/week3-2.cpp
@@ -0,0 +1,16 @@
+// programmers week3-2
+// 무인도 여행
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<string> maps) {
+    vector<int> answer;
+    return answer;
+}
+
+int main() {
+
+
+}
