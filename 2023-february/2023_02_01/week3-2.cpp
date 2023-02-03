31ff7156b86410ac30d25dd3cb04fe1754bd1879
suddiyo <suddii01@sju.ac.kr>
2023-02-03 22:11:12 +0900

    2023_02_01

--- 2023-february/2023_02_01/week3-2.cpp
+++ 2023-february/2023_02_01/week3-2.cpp
@@ -1,17 +1,19 @@
 // programmers week3-2
 // 무인도 여행
-// BFS (시간초과)
+// BFS
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int island[100][100];
+bool visited[100][100];
 vector<pair<int, int>> V;
 vector<int> days;
 
 int di[4] = {0, 0, 1, -1};
 int dj[4] = {1, -1, 0, 0};
+
 
 vector<int> solution(vector<string> maps) {
     for (int i = 0; i < maps.size(); i++) {
@@ -23,7 +25,8 @@ vector<int> solution(vector<string> maps) {
     }
 
     for (auto ele: V) {
-        if (!island[ele.first][ele.second]) continue;
+        if (!island[ele.first][ele.second] || visited[ele.first][ele.second]) continue;
+
         int day = 0;
         queue<pair<int, int>> Q;
         Q.push(ele);
@@ -31,19 +34,21 @@ vector<int> solution(vector<string> maps) {
             pair<int, int> cur = Q.front();
             Q.pop();
             day += island[cur.first][cur.second];
-            island[cur.first][cur.second] = 0;
+            visited[cur.first][cur.second] = true;
 
             for (int idx = 0; idx < 4; idx++) {
                 int new_i = cur.first + di[idx];
                 int new_j = cur.second + dj[idx];
                 if (new_i < 0 || new_i > maps.size() - 1 || new_j < 0 || new_j > maps[0].size() - 1) continue;
-                if (!island[new_i][new_j]) continue;
+                if (!island[new_i][new_j] || visited[new_i][new_j]) continue;
+                visited[new_i][new_j] = true;
                 Q.push({new_i, new_j});
             }
         }
         days.push_back(day);
     }
     sort(days.begin(), days.end());
+
     if (days.size() == 0) days.push_back(-1);
     return days;
 }
