b61c9beeba1133703d66b49b8e7f852435f20468
suddiyo <suddii01@sju.ac.kr>
2023-02-19 18:32:51 +0900

    2023_02_14

--- 2023-february/2023_02_14/week5-1.cpp
+++ 2023-february/2023_02_14/week5-1.cpp
@@ -1,4 +1,19 @@
-//
-// Created by 안수경 on 2023/02/14.
-//
+// programmers week5-1
+// 점프와 순간 이동
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n) {
+    int ans = 0;
+
+    while (n) {
+        if (n % 2 == 0) n /= 2;
+        else {
+            n -= 1;
+            ans++;
+        }
+    }
+    return ans;
+}
