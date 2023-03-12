06c801ff20ecae33d6cf7955e96185901018b644
suddiyo <suddii01@sju.ac.kr>
2023-03-12 23:56:22 +0900

    2023_03_12

---
+++ 2023-march/2023_03_12/14888.cpp
@@ -0,0 +1,26 @@
+// baekjoon 14888
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    vector<int> A;
+    vector<string> op;
+
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        A.push_back(tmp);
+    }
+    for (int i = 0; i < N; i++) {
+        string tmp;
+        cin >> tmp;
+        op.push_back(tmp);
+    }
+
+
+}
